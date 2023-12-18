53c16ce13e15d1f55c1e116cd7668e8fcfd4971a
Nick Harper <bignickharper@gmail.com>
2023-12-17 19:37:35 -0800

    Refactor PinochleView.vue to include PinochleScoreAnalyzer component and shuffleDeck button

---
+++ src/components/PinochleScoreAnalyzer.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="pinochle-score-analyzer container">
+    <h3>Hand Analysis</h3>
+    <div class="score">
+      <p>Total Score: {{ calculateScore() }}</p>
+      <!-- Additional analysis and breakdown can be displayed here -->
+    </div>
+    <!-- You can iterate over the hand and display each card with Bootstrap Icons -->
+    <div class="row">
+      <div class="col-1" v-for="card in hand" :key="card.code">
+        <img
+          :src="drawnCardImage(card.code)"
+          alt=""
+          class="img-fluid rounded shadow"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PinochleScoreAnalyzer",
+  props: {
+    hand: {
+      type: Array,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      // You can define additional data properties if needed
+    };
+  },
+  computed: {
+    // Computed properties for analyzing the hand
+  },
+  methods: {
+    // Methods for specific calculations
+    calculateScore() {
+      let score = 0;
+      // Logic to calculate the score based on the hand
+      // This might involve checking for specific combinations, sequences, etc.
+      return score;
+    },
+    drawnCardImage(code) {
+      return require(`@/assets/img/cards/${code}.png`);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.playing-card {
+  width: 100px;
+  height: 150px;
+  margin: 5px;
+}
+</style>
--- src/views/PinochleView.vue
+++ src/views/PinochleView.vue
@@ -1,15 +1,45 @@
 <template>
-  <div></div>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <PinochleScoreAnalyzer :hand="hand" />
+      </div>
+    </div>
+    <div class="row">
+      <div class="col text-center">
+        <button class="btn btn-primary mt-3" @click="shuffleDeck">
+          Deal New Hand
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import PinochleScoreAnalyzer from "@/components/PinochleScoreAnalyzer.vue";
+import { mapGetters, mapActions } from "vuex";
 export default {
   name: "PinochleView",
+
   data() {
     return {};
   },
-  components: {},
-  computed: {},
-  methods: {},
+  components: {
+    PinochleScoreAnalyzer,
+  },
+  computed: {
+    ...mapGetters(["getShuffledDeck"]),
+    hand() {
+      const deck = this.getShuffledDeck;
+      const hand = [];
+      for (let i = 0; i < 12; i++) {
+        hand.push(deck.pop());
+      }
+      return hand;
+    },
+  },
+  methods: {
+    ...mapActions(["shuffleDeck"]),
+  },
 };
 </script>
